2312ab6896d72843927c5062877c1f755526c953
fernando Lopes <elfercape@gmail.com>
2024-10-04 12:11:11 -0300

    Se realiza scalffold y se actualiza seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+50.times do
+    Tweet.create(
+      username: Faker::Name.name,
+      description: Faker::Lorem.sentence,
+    )
+  end
